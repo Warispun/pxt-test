da14ce0ce2bbf23fa95b7ffdda352a8b693ecbec
Warispun <Warispun@users.noreply.github.com>
2018-10-01 22:35:11 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -49,15 +49,25 @@ namespace whaleysans {
         img.showImage(0, 10);
     }
     
-    //% blockId = "function with argument" block="function %dat"
-    export function function(dat: number): void {
-        img = images.createImage(`
-    . . . . .
-    . . . . .
-    . # # # .
-    . . . . .
-    . . . . .
-    `)
-        img.showImage(0,10)
+    /**
+     * show a number
+     * @param dat is number will be show, eg: 10
+     */
+    //% blockId="show_whaleysans_number" block="show a whaleysans number %dat"
+    //% dat.min=0 dat.max=99
+    export function showNumber(dat: number): void {
+        if(dat<0)
+            dat=0;
+
+        let a = FONT[Math.idiv(dat, 10) % 10];
+        let b = FONT[dat % 10];
+
+        for (let i = 0; i < 5; i++) {
+            img.setPixel(0, i, 1 == a[i * 2])
+            img.setPixel(1, i, 1 == a[i * 2 + 1])
+            img.setPixel(3, i, 1 == b[i * 2])
+            img.setPixel(4, i, 1 == b[i * 2 + 1])
+        }
+        img.showImage(0, 10);
     }
 }
